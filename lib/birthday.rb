11252bc3da2f5648d560ddc329d8e3a256332e80
Awoh Negheie <awohnegheie@gmail.com>
2019-05-09 23:51:55 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,12 +1,10 @@
-def 
+def
   birthday_kids = {
-   "Timmy" => 9, 
-   "Sarah" => 6, 
+   "Timmy" => 9,
+   "Sarah" => 6,
    "Amanda" => 27 }
 end
 
 def happy_birthday(birthday_kids)
   # add your code snippet here!
 end
-
-
